f6054f4f3be5bbe592b5771918a5beadbab5ccfe
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-10-15 14:18:59 -0700

    hack apply

--- src/Control/Apply.js
+++ src/Control/Apply.js
@@ -1,14 +1,19 @@
-"use strict";
+package control;
 
-exports.arrayApply = function (fs) {
-  return function (xs) {
-    var result = [];
-    var n = 0;
-    for (var i = 0, l = fs.length; i < l; i++) {
-      for (var j = 0, k = xs.length; j < k; j++) {
-        result[n++] = fs[i](xs[j]);
-      }
+class ApplyForeign {
+
+    public static function arrayApply (fs) {
+        return [];
+      //return function (xs) {
+        //var result = [];
+        //var n = 0;
+        //for (var i = 0, l = fs.length; i < l; i++) {
+          //for (var j = 0, k = xs.length; j < k; j++) {
+            //result[n++] = fs[i](xs[j]);
+          //}
+        //}
+        //return result;
+      //};
     }
-    return result;
-  };
-};
+
+}
