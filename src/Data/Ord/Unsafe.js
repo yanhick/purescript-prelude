d6093603608ac8591457fecdd5455aa31a0b1ecf
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-11-10 10:55:03 -0800

    hack unsafe foreign

--- src/Data/Ord/Unsafe.js
+++ src/Data/Ord/Unsafe.js
@@ -1,13 +1,17 @@
-"use strict";
+package data.ord;
 
-exports.unsafeCompareImpl = function (lt) {
-  return function (eq) {
-    return function (gt) {
-      return function (x) {
-        return function (y) {
-          return x < y ? lt : x === y ? eq : gt;
+class UnsafeForeign {
+    public static function unsafeCompareImpl (lt) {
+      return function (eq) {
+        return function (gt) {
+          return function (x) {
+            return function (y) {
+              return x < y ? lt : x == y ? eq : gt;
+            };
+          };
         };
       };
     };
-  };
-};
+}
+
+
