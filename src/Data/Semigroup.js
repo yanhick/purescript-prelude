2708da4fd61b2a3deaef6fd337e704ab05d95a06
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-11-10 10:53:13 -0800

    hack semigroup

--- src/Data/Semigroup.js
+++ src/Data/Semigroup.js
@@ -1,15 +1,17 @@
-"use strict";
+package data;
 
-exports.concatString = function (s1) {
-  return function (s2) {
-    return s1 + s2;
-  };
-};
+class SemigroupForeign {
+    public static function concatString (s1) {
+      return function (s2) {
+        return s1 + s2;
+      };
+    };
 
-exports.concatArray = function (xs) {
-  return function (ys) {
-    if (xs.length === 0) return ys;
-    if (ys.length === 0) return xs;
-    return xs.concat(ys);
-  };
-};
+    public static function concatArray (xs) {
+      return function (ys) {
+        if (xs.length == 0) return ys;
+        if (ys.length == 0) return xs;
+        return xs.concat(ys);
+      };
+    };
+}
