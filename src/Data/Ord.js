9ffeff63846837e7d09186954c7d34e04c905828
Yannick Dominguez <yannick.dominguez@gmail.com>
2016-11-10 11:01:34 -0800

    hack ord

--- src/Data/Ord.js
+++ src/Data/Ord.js
@@ -1,27 +1,30 @@
-"use strict";
+package data;
 
-exports.ordArrayImpl = function (f) {
-  return function (xs) {
-    return function (ys) {
-      var i = 0;
-      var xlen = xs.length;
-      var ylen = ys.length;
-      while (i < xlen && i < ylen) {
-        var x = xs[i];
-        var y = ys[i];
-        var o = f(x)(y);
-        if (o !== 0) {
-          return o;
-        }
-        i++;
-      }
-      if (xlen === ylen) {
-        return 0;
-      } else if (xlen > ylen) {
-        return -1;
-      } else {
-        return 1;
-      }
+class OrdForeign {
+    public static function ordArrayImpl (f) {
+      return function (xs) {
+        return function (ys) {
+          var i = 0;
+          var xlen = xs.length;
+          var ylen = ys.length;
+          while (i < xlen && i < ylen) {
+            var x = xs[i];
+            var y = ys[i];
+            var o = f(x)(y);
+            if (o != 0) {
+              return o;
+            }
+            i++;
+          }
+          if (xlen == ylen) {
+            return 0;
+          } else if (xlen > ylen) {
+            return -1;
+          } else {
+            return 1;
+          }
+        };
+      };
     };
-  };
-};
+}
+
